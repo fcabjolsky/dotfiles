bacdfe869b20dc3e2278b74f43d92875d1790e39
Felipe <felipe@frontm.com>
2023-04-23 21:15:25 -0300

    Add keymaps for fugitive

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git,{ desc = "Git fugitive" } )
 vim.keymap.set("n", "<leader>gbm", function() vim.cmd.Git({ args = { "blame" } }) end, { desc = "[G]it [B]lame [M]e" })
+vim.keymap.set("n", "gj", "<cmd>diffget //2<CR>", {desc = "Get changes from right"})
+vim.keymap.set("n", "gf", "<cmd>diffget //3<CR>", {desc = "Get changes from left"})
