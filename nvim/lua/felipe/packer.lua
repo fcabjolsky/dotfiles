c0fd166f0c40be6e27275d4a7ad313087cc63d9a
Felipe <felipe@frontm.com>
2023-12-10 20:06:51 -0300

    chore: update harpoon to harpoon2

--- nvim/lua/felipe/packer.lua
+++ nvim/lua/felipe/packer.lua
@@ -7,34 +7,37 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        'nvim-telescope/telescope.nvim',
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
     use('doums/darcula')
     use({ 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } })
     use('nvim-treesitter/playground')
-    use('theprimeagen/harpoon')
+    use { 'theprimeagen/harpoon',
+        branch = "harpoon2",
+        requires = { { "nvim-lua/plenary.nvim" } }
+    }
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
     use {
         'VonHeikemen/lsp-zero.nvim',
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            { 'williamboman/mason.nvim' }, -- Optional
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' }, -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'hrsh7th/cmp-buffer' }, -- Optional
-            { 'hrsh7th/cmp-path' }, -- Optional
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'hrsh7th/cmp-buffer' },       -- Optional
+            { 'hrsh7th/cmp-path' },         -- Optional
             { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' }, -- Required
+            { 'L3MON4D3/LuaSnip' },             -- Required
             { 'rafamadriz/friendly-snippets' }, -- Optional
         }
     }
@@ -50,6 +53,7 @@ return require('packer').startup(function(use)
     use { 'nvim-tree/nvim-web-devicons' }
     use { 'MunifTanjim/nui.nvim' }
     -- use { '~/Documents/Desa/lua/git-push/' }
+    -- use { '~/Documents/Desa/lua/git-blame' }
     use { 'fcabjolsky/git-blame' }
     use { 'fcabjolsky/git-push' }
     use { 'ThePrimeagen/git-worktree.nvim' }
